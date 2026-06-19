e64fc7fabb58c28ac3da36a87767bec5fe5e2308
Lazare Kassi <sales@panamvalves.co.uk>
2026-06-19 10:19:30 +0100

    feat:added-event-args

---
+++ Bank_System.Console/Events/TransactionEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class TransactionEvent
+    {
+        public event EventHandler<TransactionEventArgs> TransactionOccurred;
+        public void OnTransactionOccurred(Guid accountId, decimal amount, TransactionType transactionType, decimal totalBalance)
+        {
+            Console.WriteLine("Transaction Occured");
+            TransactionOccurred?.Invoke(this, new TransactionEventArgs
+            {
+                AccountId = accountId,
+                Amount = amount,
+                TransactionType = transactionType,
+                TotalBalance = totalBalance
+            });
+        }
+    }
+}
---
+++ Bank_System.Console/Events/TransactionEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class TransactionEventArgs :EventArgs
+    {
+        public Guid AccountId { get; set; }
+        public decimal Amount { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public decimal TotalBalance { get; set; }
+    }
+}
